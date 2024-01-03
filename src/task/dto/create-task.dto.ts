7c63d234562980c5c303f70c069bd8059b83b58b
bipinparajuli <bipinprjl@gmail.com>
2024-01-03 07:00:27 +0545

    initial commit

---
+++ src/task/dto/create-task.dto.ts
@@ -0,0 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateTaskDto {
+
+  @ApiProperty()
+  title: string;
+
+  @ApiProperty()
+  description: string;
+
+  @ApiProperty()
+  status: string;
+}
+
